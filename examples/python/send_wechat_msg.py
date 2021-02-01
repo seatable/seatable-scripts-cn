0091ccd8b624003f3622e1733f05c1aa244daa43
r350178982 <32759763+r350178982@users.noreply.github.com>
2021-02-01 21:45:45 +0800

    Update send_wechat_msg.py

--- examples/python/send_wechat_msg.py
+++ examples/python/send_wechat_msg.py
@@ -4,12 +4,14 @@ from seatable_api import context
 """
 该脚本展示利用企业微信群组机器人进行表格中数据的发送。
 """
-WEBHOOK_URL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=39f5cb53-b909-4e30-9d99-75782"  # 建立群机器人之后自动生成的webhook地址，可以通过post请求该地址进行消息发送
+# 建立群机器人之后自动生成的webhook地址，可以通过post请求该地址进行消息发送
+WEBHOOK_URL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=39f5cb53-b909-4e30-9d99-75782"
 HEADERS = {
     "Content-Type": "application/json"
 }
 
-COLUMNS =  ['customer_name', 'user_limit'] # 发送消息的列
+# 发送消息的列
+COLUMNS =  ['customer_name', 'user_limit']
 
 # 构造发送文本消息的json数据结构
 def json_text_msg(msg):
